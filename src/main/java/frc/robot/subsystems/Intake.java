d1650b0602ccc48b239c82d69eff8d9e6269b8a5
frc3175Team <frc3175@gmail.com>
2026-01-28 20:01:51 -0500

    added motion magic for rack extension

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,15 +4,20 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import com.ctre.phoenix6.hardware.CANrange;
+
+import com.ctre.phoenix6.hardware.CANcoder;
 
 public class  Intake extends SubsystemBase {
   
@@ -21,9 +26,19 @@ public class  Intake extends SubsystemBase {
 TalonFX m_rackMotor;
 TalonFX m_intakeleftMotor;
 TalonFX m_intakerightMotor;
-VelocityDutyCycle intakeVelocity; 
+VelocityDutyCycle intakeVelocity;
+
+
 
 DutyCycleOut intakePercentOutput;
+
+MotionMagicVoltage m_motmag;
+
+PositionDutyCycle m_PositionDutyCycle;
+CANcoder m_canCoder;
+double cancoderzero = 0.5;
+double cancoderoffset = 0;
+
 
 
 
@@ -33,14 +48,17 @@ public Intake() {
     m_intakerightMotor = new TalonFX(Constants.IntakeConstants.RIGHTMOTORID, Constants.CANIVORE);
 
     //TODO: add motion magic for rack
-    //TODO: add neutral mode
+  
      
     final TalonFXConfiguration rackConfiguration = new TalonFXConfiguration();
     rackConfiguration.CurrentLimits.withStatorCurrentLimitEnable(true);
     rackConfiguration.CurrentLimits.withStatorCurrentLimit(Constants.IntakeConstants.CurrentLimit);
+    rackConfiguration.withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
     final TalonFXConfiguration rollerConfiguration = new TalonFXConfiguration();
     rollerConfiguration.CurrentLimits.withStatorCurrentLimitEnable(true);
     rollerConfiguration.CurrentLimits.withStatorCurrentLimit(Constants.IntakeConstants.CurrentLimit);
+    rackConfiguration.withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast));
+
    
 
     m_rackMotor.getConfigurator().apply(rackConfiguration, 0.050);
@@ -48,6 +66,13 @@ public Intake() {
     m_intakerightMotor.getConfigurator().apply(rollerConfiguration, 0.050);
 
     intakeVelocity = new VelocityDutyCycle(0);
+
+
+    m_motmag = new MotionMagicVoltage(0);
+
+    configure();
+
+    
 
     // periodic, run Motion Magic with slot 0 configs,
   }
@@ -57,6 +82,8 @@ public Intake() {
     
     SmartDashboard.putNumber("rackpose", 0);
     SmartDashboard.putNumber("Roller vel", m_intakeleftMotor.getVelocity().getValueAsDouble());
+    
+    m_motmag.Slot = 0;
   
   }
 
@@ -64,10 +91,6 @@ public Intake() {
     intakeVelocity.Velocity = velocity * 5;
         m_intakeleftMotor.setControl(intakeVelocity);
         m_intakerightMotor.setControl(intakeVelocity);
-
-
-    
-    
   }
 
 
@@ -75,68 +98,30 @@ public Intake() {
     intakeVelocity.Velocity = -velocity * 5;
         m_intakeleftMotor.setControl(intakeVelocity);
         m_intakerightMotor.setControl(intakeVelocity);
+  }
+
+public void setpos(double postion){
+
+    m_rackMotor.setControl(m_motmag.withPosition(postion));
 
 
-   
-    
+  }
+
+  public void extendintake(){
+    m_rackMotor.setControl(m_motmag.withPosition(Constants.IntakeConstants.RACKMAX));
+  }
+
+   public void retractintake(){
+    m_rackMotor.setControl(m_motmag.withPosition(Constants.IntakeConstants.RACKHOME));
   }
 
 
 
-    
-
-}
 
 
 
 
-
-/*// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.subsystems;
-
-import com.ctre.phoenix6.configs.CANcoderConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import static edu.wpi.first.units.Units.Rotations; 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-
-
-public class Hopper extends SubsystemBase {
-  
-
-MotionMagicVoltage m_motmag;
-
-PositionDutyCycle m_PositionDutyCycle;
-TalonFX m_motor;
-CANcoder m_canCoder;
-double nudge = 0;
-double cancoderzero = 0.5;
-double cancoderoffset = 0;
-
-
-public Hopper() {
-    m_motor = new TalonFX(Constants.WristConstants.MOTORID , Constants.CANIVORE);
-    // m_canCoder = new CANcoder(Constants.WristConstants.CANCODERID, Constants.CANIVORE);
-
-    // cancoderoffset = m_canCoder.getAbsolutePosition().getValueAsDouble() - cancoderoffset;
-
-
-    
-
-    m_motmag = new MotionMagicVoltage(0);
-
+  public void configure(){
     var talonFXConfigs = new TalonFXConfiguration();
     
     // var canCoderConfigs = new CANcoderConfiguration();
@@ -144,7 +129,7 @@ public Hopper() {
     var slot0Configs = talonFXConfigs.Slot0;
     
 
-    slot0Configs.kP = 2; // change as needed
+    slot0Configs.kP = 0.2; // change as needed
     slot0Configs.kI = 0;
     slot0Configs.kD = 0;
 
@@ -157,50 +142,15 @@ public Hopper() {
 
     m_motmag.EnableFOC = true;
 
+  }
 
 
-    // talonFXConfigs.Feedback.FeedbackRemoteSensorID = m_canCoder.getDeviceID();
-    // talonFXConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.SyncCANcoder;
-    // talonFXConfigs.Feedback.SensorToMechanismRatio = 1.0;
-    // talonFXConfigs.Feedback.RotorToSensorRatio = -60.7639;
-
-    // canCoderConfigs.MagnetSensor.withAbsoluteSensorDiscontinuityPoint(1);
-    // canCoderConfigs.MagnetSensor.withSensorDirection(SensorDirectionValue.CounterClockwise_Positive);
-    // canCoderConfigs.MagnetSensor.withMagnetOffset(Rotations.of(0.1));
 
     
-    m_motor.getConfigurator().apply(talonFXConfigs, 0.050);
-    // m_canCoder.getConfigurator().apply(canCoderConfigs, 0.050);
-    m_motor.setNeutralMode(NeutralModeValue.Brake);
-    // periodic, run Motion Magic with slot 0 configs,
-  }
-  
-  @Override
-  public void periodic() {
-    m_motmag.Slot = 0;
-    SmartDashboard.putNumber("wrist angle", getpose());
-  }
-
-  public Double getpose(){ // if you need negative pos, you need to change this
-  return  m_motor.getPosition().getValueAsDouble();
-
- 
-
-  }
-
-  public void setangle(double angle){
-
-    m_motor.setControl(m_motmag.withPosition(angle+nudge));
-
-
-  }
-  public void setnudge(double change){
-    nudge = nudge + change;
-  }
 
 }
 
 
 
 
- */
+
