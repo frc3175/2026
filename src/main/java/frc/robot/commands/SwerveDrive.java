2c05a99426e3dac405f6d311b7620f42947c7f32
frc3175Team <frc3175@gmail.com>
2026-04-07 23:02:42 -0400

    allow spin

--- src/main/java/frc/robot/commands/SwerveDrive.java
+++ src/main/java/frc/robot/commands/SwerveDrive.java
@@ -144,7 +144,7 @@ public class SwerveDrive extends Command {
         
             double rAxisActual = rAxisSquared * MaxAngularRate * -1;
        
-        if (Math.abs(xAxisSquared) < 0.1 && Math.abs(yAxisSquared) < 0.1 && m_isShooting.getAsBoolean()) {
+        if (Math.abs(xAxisSquared) < 0.1 && Math.abs(yAxisSquared) < 0.1 && Math.abs(rAxisActual) < 0.1 && m_isShooting.getAsBoolean()) {
             m_swerveDrivetrain.setControl(new SwerveRequest.SwerveDriveBrake());
         } else if(!m_isAligning.getAsBoolean()) {
             m_swerveDrivetrain.setControl(
